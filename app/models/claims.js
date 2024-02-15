a2e2fb830f91709b422ad40a67b0dfa6cbf9d8f2
Simon Whatley <simonwhatley@gmail.com>
2024-02-15 11:33:54 +0000

    Add insertOne and updateOne function definitions

--- app/models/claims.js
+++ app/models/claims.js
@@ -24,3 +24,91 @@ exports.findMany = (params) => {
 
   return claims
 }
+
+exports.findOne = (params) => {
+  const claims = this.findMany(params)
+  let claim = {}
+
+  if (params.claimId) {
+    claim = claims.find(claim => claim.id === params.claimId)
+  }
+
+  return claim
+}
+
+exports.insertOne = (params) => {
+  let claim = {}
+
+  if (params.organisationId) {
+    claim.id = uuid()
+
+    claim.organisationId = params.organisationId
+
+    if (params.claim.reference) {
+      claim.reference = params.claim.reference.toUpperCase()
+    }
+
+    if (params.claim.provider) {
+      claim.provider = params.claim.provider
+    }
+
+    if (params.claim.mentors) {
+      claim.mentors = params.claim.mentors
+    }
+
+    if (params.claim.status) {
+      claim.status = params.claim.status
+    }
+
+    claim.createdAt = new Date()
+
+    const filePath = directoryPath + '/' + claim.id + '.json'
+
+    // create a JSON sting for the submitted data
+    const fileData = JSON.stringify(claim)
+
+    // write the JSON data
+    fs.writeFileSync(filePath, fileData)
+  }
+
+  return claim
+}
+
+exports.updateOne = (params) => {
+  let claim = {}
+
+  if (params.organisationId && params.claimId) {
+    claim = this.findOne({
+      organisationId: params.organisationId,
+      claimId: params.claimId,
+    })
+
+    if (params.claim.provider) {
+      claim.provider = params.claim.provider
+    }
+
+    if (params.claim.mentors) {
+      claim.mentors = params.claim.mentors
+    }
+
+    if (params.claim.status) {
+      claim.status = params.claim.status
+    }
+
+    claim.updatedAt = new Date()
+
+    const filePath = directoryPath + '/' + claim.id + '.json'
+
+    // create a JSON sting for the submitted data
+    const fileData = JSON.stringify(claim)
+
+    // write the JSON data
+    fs.writeFileSync(filePath, fileData)
+  }
+
+  return claim
+}
+
+exports.deleteOne = (params) => {
+
+}
