8d90c08e61ad33cb052f74da160782a4245d9cc4
Simon Whatley <simonwhatley@gmail.com>
2024-03-06 10:54:38 +0000

    Add funding information

--- app/controllers/support/organisation-claims.js
+++ app/controllers/support/organisation-claims.js
@@ -4,6 +4,7 @@ const organisationModel = require('../../models/organisations')
 
 const Pagination = require('../../helpers/pagination')
 const claimHelper = require('../../helpers/claims')
+const fundingHelper = require('../../helpers/funding')
 const mentorHelper = require('../../helpers/mentors')
 
 /// ------------------------------------------------------------------------ ///
@@ -319,6 +320,37 @@ exports.new_claim_hours_post = (req, res) => {
 exports.new_claim_check_get = (req, res) => {
   const organisation = organisationModel.findOne({ organisationId: req.params.organisationId })
   const position = req.session.data.claim.mentors.length - 1
+
+  // console.log('Mentors',req.session.data.claim.mentors)
+
+  const mentorHours = req.session.data.claim.mentors.map(mentor => {
+    if (mentor.hours === 'other') {
+      return parseInt(mentor.otherHours)
+    } else {
+      return parseInt(mentor.hours)
+    }
+  })
+
+  // console.log('Mentor hours:', mentorHours)
+
+  const initialHours = 0
+
+  const totalHours = mentorHours.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialHours
+  )
+
+  // console.log('Total hours:', totalHours)
+
+  const fundingRate = fundingHelper.getFundingRate(organisation.location.districtAdministrativeCode)
+
+  // console.log('Funding rate:', fundingRate)
+
+  const totalAmount = fundingRate * totalHours
+
+  // console.log('Total amount:', totalAmount)
+
+  req.session.data.claim.totalAmount = totalAmount
 
   res.render('../views/support/organisations/claims/check-your-answers', {
     organisation,
