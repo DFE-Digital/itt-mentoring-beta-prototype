d53f26aea6c9eee60fd879385cef4fab5140560b
Simon Whatley <simonwhatley@gmail.com>
2024-09-05 13:11:22 +0100

    Upload payments file

--- app/controllers/support/payments.js
+++ app/controllers/support/payments.js
@@ -1,12 +1,16 @@
-
 const claimModel = require('../../models/claims')
 const paymentModel = require('../../models/payments')
+
+const paymentHelper = require('../../helpers/payments')
+
+const fs = require('fs')
+const csv = require('csv-string')
 
 exports.list_claims_get = (req, res) => {
   res.render('../views/support/claims/payments/index', {
     actions: {
       export: `/support/claims/payments/export`,
-      response: `/support/claims/payments/response`,
+      response: `/support/claims/payments/import`,
       view: `/support/claims/payments`
     }
   })
@@ -30,6 +34,8 @@ exports.export_claims_get = (req, res) => {
 }
 
 exports.export_claims_post = (req, res) => {
+  console.log(req.file);
+
   const errors = []
 
   if (errors.length) {
@@ -71,30 +77,114 @@ exports.export_claims_post = (req, res) => {
   }
 }
 
-exports.claims_response_get = (req, res) => {
-  res.render('../views/support/claims/payments/response', {
+exports.import_claims_get = (req, res) => {
+  const claims = claimModel
+    .findMany({ })
+    .filter(claim => claim.status === 'payment_pending')
+
+  const hasClaims = !!claims.length
+
+  res.render('../views/support/claims/payments/import', {
+    hasClaims,
     actions: {
-      save: `/support/claims/payments/response`,
+      save: `/support/claims/payments/import`,
       back: `/support/claims/payments`,
       cancel: `/support/claims/payments`
     }
   })
 }
 
-exports.claims_response_post = (req, res) => {
+exports.import_claims_post = (req, res) => {
   const errors = []
 
-  if (errors.length) {
+  console.log(req.file)
+  console.log(req.file.mimetype)
 
-    res.render('../views/support/claims/payments/response', {
+  if (!req.file) {
+    const error = {}
+    error.fieldName = 'payments'
+    error.href = '#payments'
+    error.text = 'Select a CSV file to upload'
+    errors.push(error)
+  } else {
+    if (req.file.mimetype !== 'text/csv') {
+      const error = {}
+      error.fieldName = 'payments'
+      error.href = '#payments'
+      error.text = 'The selected file must be a CSV'
+      errors.push(error)
+      // delete the incorrect file
+      fs.unlinkSync(req.file.path)
+    } else if (!req.file.size) {
+      const error = {}
+      error.fieldName = 'payments'
+      error.href = '#payments'
+      error.text = 'The selected file is empty'
+      errors.push(error)
+      // delete the incorrect file
+      fs.unlinkSync(req.file.path)
+    }
+  }
+
+  if (errors.length) {
+    const claims = claimModel
+      .findMany({ })
+      .filter(claim => claim.status === 'payment_pending')
+
+    const hasClaims = !!claims.length
+
+    res.render('../views/support/claims/payments/import', {
+      hasClaims,
       actions: {
-        save: `/support/claims/payments/response`,
+        save: `/support/claims/payments/import`,
         back: `/support/claims/payments`,
         cancel: `/support/claims/payments`
       },
       errors
     })
   } else {
+    // get the CSV delimiter
+    const delimiter = csv.detect(req.file.path)
+    // read the raw CSV data
+    const raw = fs.readFileSync(req.file.path, 'utf8')
+    // parse the CSV data
+    let payments = []
+    csv.readAll(raw, delimiter, data => {
+      payments = data
+    })
+    // put the data into the session for use later
+    req.session.data.payments = paymentHelper.parseData(payments)
+
+    res.redirect('/support/claims/payments/review')
+  }
+}
+
+exports.review_claims_get = (req, res) => {
+  res.render('../views/support/claims/payments/review', {
+    actions: {
+      save: `/support/claims/payments/review`,
+      back: `/support/claims/payments/import`,
+      cancel: `/support/claims/payments`
+    }
+  })
+}
+
+exports.review_claims_post = (req, res) => {
+  const errors = []
+
+  if (errors.length) {
+
+    res.render('../views/support/claims/payments/review', {
+      actions: {
+        save: `/support/claims/payments/review`,
+        back: `/support/claims/payments/import`,
+        cancel: `/support/claims/payments`
+      },
+      errors
+    })
+  } else {
+
+
     req.flash('success', 'ESFA reponse uploaded')
     res.redirect('/support/claims/payments')
   }
