ad0ddcdb22b6b2bba2c18305c24410a80acecec2
Simon Whatley <simonwhatley@gmail.com>
2024-09-06 20:00:22 +0100

    Add parse claim data function

--- app/helpers/claims.js
+++ app/helpers/claims.js
@@ -87,3 +87,35 @@ exports.getProviderMentorTotalHours = (params) => {
 
   return totalHours
 }
+
+exports.parseData = (claims) => {
+  const items = []
+
+  claims.forEach((claim) => {
+    const item = {}
+
+    item.claim_reference = claim.reference
+    item.school_urn = claim.school.urn
+    item.school_name = claim.school.name
+
+    if (claim.school?.location?.localAuthorityCode) {
+      item.local_authority_code = claim.school.location.localAuthorityCode
+    } else {
+      item.local_authority_code = 'UNKNOWN'
+    }
+
+    if (claim.school?.establishmentType) {
+      item.establishment_type_code = claim.school.establishmentType
+    } else {
+      item.establishment_type_code = 'UNKNOWN'
+    }
+
+    item.claim_amount = claim.totalAmount
+    item.date_submitted = claim.submittedAt
+    item.claim_status = claim.status
+
+    items.push(item)
+  })
+
+  return items
+}
