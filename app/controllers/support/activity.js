4229e028674273992fa6ee881bc622a182f8c55e
Simon Whatley <simonwhatley@gmail.com>
2024-10-16 12:05:17 +0100

    Update activity.js

--- app/controllers/support/activity.js
+++ app/controllers/support/activity.js
@@ -1,5 +1,18 @@
-exports.list_activity_get = (req, res) => {
-  res.render('../views/support/claims/activity/index', {
+const activityLogModel = require('../../models/activity')
 
+const Pagination = require('../../helpers/pagination')
+
+exports.list_activity_get = (req, res) => {
+  let activity = activityLogModel.findMany()
+
+  activity.sort((a, b) => {
+    return new Date(b.createdAt) - new Date(a.createdAt)
+  })
+
+  res.render('../views/support/claims/activity/index', {
+    activity,
+    actions: {
+      show: `/support/claims/activity`
+    }
   })
 }
