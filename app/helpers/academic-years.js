95e0c27a1e9da6c15ea95fd10557bc082a3226cf
Simon Whatley <simonwhatley@gmail.com>
2024-10-22 10:52:40 +0100

    Update academic year helper with options and label

--- app/helpers/academic-years.js
+++ app/helpers/academic-years.js
@@ -1,4 +1,5 @@
 const claimWindows = require('../data/dist/settings/claim-windows')
+const academicYears = require('../data/dist/settings/academic-years')
 
 exports.getAcademicYear = (date) => {
   // find the academic year for a given date
@@ -16,6 +17,37 @@ exports.getAcademicYear = (date) => {
   }
 }
 
-exports.getAcademicYearLabel = (code) => {
+exports.getAcademicYearOptions = (selectedItem) => {
+  const items = []
 
+  academicYears.forEach((academicYear, i) => {
+    const item = {}
+
+    item.text = academicYear.name
+    item.value = academicYear.code
+    item.id = academicYear.id
+    item.checked = (selectedItem && selectedItem.includes(academicYear.code)) ? 'checked' : ''
+
+    if (academicYear.active) {
+      items.push(item)
+    }
+  })
+
+  items.sort((a, b) => {
+    return a.text.localeCompare(b.text)
+  })
+
+  return items
+}
+
+exports.getAcademicYearLabel = (code) => {
+  let label
+
+  const academicYear = academicYears.find((academicYear) => academicYear.code === code)
+
+  if (academicYear) {
+    label = academicYear.name
+  }
+
+  return label
 }
