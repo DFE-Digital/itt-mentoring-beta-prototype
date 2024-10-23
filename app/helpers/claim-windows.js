baf14460258defe96738a80d24590d8ffae4a4a6
Simon Whatley <simonwhatley@gmail.com>
2024-10-23 14:59:32 +0100

    Add isClaimWindowOpen function

--- app/helpers/claim-windows.js
+++ app/helpers/claim-windows.js
@@ -14,3 +14,20 @@ exports.getCurrentClaimWindow = () => {
 
   return claimWindows[0]
 }
+
+exports.isClaimWindowOpen = () => {
+  const currentDate = new Date()
+
+  // iterate through each item in the claim windows array
+  for (let window of claimWindows) {
+    const opensAt = new Date(window.opensAt)
+    const closesAt = new Date(window.closesAt)
+
+    // check if current date is between opensAt and closesAt
+    if (currentDate >= opensAt && currentDate <= closesAt) {
+      return true
+    }
+  }
+
+  return false
+}
